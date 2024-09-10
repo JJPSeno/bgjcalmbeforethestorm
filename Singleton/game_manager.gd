412f37e61cff1e5dec60bba2cb1429ea5db521ce
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2024-09-10 23:52:01 +0800

    Update player controls

---
+++ Singleton/game_manager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
