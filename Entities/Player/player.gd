693c8a08b635fc5a5a2e2fbe6076c6ae8c509af0
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2024-09-13 23:47:28 +0800

    added some more interactable entities and npcs

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -17,75 +17,75 @@ enum FacingDirection {UP, RIGHT, DOWN, LEFT}
 
 
 func _ready() -> void:
-    weapon.monitorable = false
-    weapon.monitoring = false
-    current_facing = FacingDirection.DOWN
+	weapon.monitorable = false
+	weapon.monitoring = false
+	current_facing = FacingDirection.DOWN
 
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("movement"):
-        if event.is_action_pressed("up"):
-            change_facing(FacingDirection.UP)
-        elif event.is_action_pressed("right"):
-            change_facing(FacingDirection.RIGHT)
-        elif event.is_action_pressed("down"):
-            change_facing(FacingDirection.DOWN)
-        elif event.is_action_pressed("left"):
-            change_facing(FacingDirection.LEFT)
-            
-    if event.is_action_pressed("hit") and can_swing:
-        swing_weapon()
+	if event.is_action_pressed("movement"):
+		if event.is_action_pressed("up"):
+			change_facing(FacingDirection.UP)
+		elif event.is_action_pressed("right"):
+			change_facing(FacingDirection.RIGHT)
+		elif event.is_action_pressed("down"):
+			change_facing(FacingDirection.DOWN)
+		elif event.is_action_pressed("left"):
+			change_facing(FacingDirection.LEFT)
+			
+	if event.is_action_pressed("hit") and can_swing:
+		swing_weapon()
 
 
 func _physics_process(delta: float) -> void:
-    direction = Input.get_vector("left","right","up","down")
-    if direction:
-        target_velocity = direction * SPEED
-        # use of framerate indipendent lerp for acceleration and deceleration
-        velocity = velocity.lerp(target_velocity, 1 - exp(-acceleration * delta))
-    else:
-        velocity = velocity.lerp(Vector2.ZERO, 1 - exp(-friction * delta))
-    move_and_slide()
+	direction = Input.get_vector("left","right","up","down")
+	if direction:
+		target_velocity = direction * SPEED
+		# use of framerate indipendent lerp for acceleration and deceleration
+		velocity = velocity.lerp(target_velocity, 1 - exp(-acceleration * delta))
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, 1 - exp(-friction * delta))
+	move_and_slide()
 
 
 func change_facing(new_facing_direction: FacingDirection) -> void:
-    # can make this more efficient using strings I think, but w/e
-    if new_facing_direction != current_facing:
-        current_facing = new_facing_direction
-        match new_facing_direction:
-            FacingDirection.UP:
-                animated_sprite_2d.animation = "face_up"
-                weapon_pivot.rotation_degrees = 180
-            FacingDirection.RIGHT:
-                animated_sprite_2d.animation = "face_right"
-                weapon_pivot.rotation_degrees = 270
-            FacingDirection.DOWN:
-                animated_sprite_2d.animation = "face_down"
-                weapon_pivot.rotation_degrees = 0
-            FacingDirection.LEFT:
-                animated_sprite_2d.animation = "face_left"
-                weapon_pivot.rotation_degrees = 90
-        
-    
+	# can make this more efficient using strings I think, but w/e
+	if new_facing_direction != current_facing:
+		current_facing = new_facing_direction
+		match new_facing_direction:
+			FacingDirection.UP:
+				animated_sprite_2d.animation = "face_up"
+				weapon_pivot.rotation_degrees = 180
+			FacingDirection.RIGHT:
+				animated_sprite_2d.animation = "face_right"
+				weapon_pivot.rotation_degrees = 270
+			FacingDirection.DOWN:
+				animated_sprite_2d.animation = "face_down"
+				weapon_pivot.rotation_degrees = 0
+			FacingDirection.LEFT:
+				animated_sprite_2d.animation = "face_left"
+				weapon_pivot.rotation_degrees = 90
+		
+	
 func swing_weapon() -> void:
-    can_swing = false
-    set_weapon_monitor(true)
-    create_weapon_active_timer()
+	can_swing = false
+	set_weapon_monitor(true)
+	create_weapon_active_timer()
 
 func create_weapon_active_timer() -> void:
-    var timer := Timer.new()
-    add_child(timer)
-    timer.wait_time = 0.1
-    timer.one_shot = true
-    timer.start()
-    timer.connect("timeout", _on_timer_timeout.bind(timer))
+	var timer := Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.1
+	timer.one_shot = true
+	timer.start()
+	timer.connect("timeout", _on_timer_timeout.bind(timer))
 
 
 func set_weapon_monitor(value: bool):
-    weapon.monitorable = value
-    weapon.monitoring = value
+	weapon.monitorable = value
+	weapon.monitoring = value
 
 func _on_timer_timeout(timer: Timer) -> void:
-    can_swing = true
-    set_weapon_monitor(false)
-    timer.queue_free()
+	can_swing = true
+	set_weapon_monitor(false)
+	timer.queue_free()
--- Systems/DialogueSystem.gd
+++ Systems/DialogueSystem.gd
@@ -5,26 +5,26 @@ var current_target:Node2D
 
 var _player:Node2D
 func _ready() -> void:
-    set_process(false)
-    _player = get_tree().get_nodes_in_group("player")[0]
+	set_process(false)
+	_player = get_tree().get_nodes_in_group("player")[0]
 
 func set_current_dialogue(diag:Dialogueable):
-    if diag == null:
-        set_process(false)
-        diag = null
-        return
-        
-    # This code snippet ensures only 1 dialogue is active
-    # At a time
-    if current_dialogue != null:
-        current_dialogue.unload()
-        
-    current_dialogue = diag
-    current_target = diag.entity
-    set_process(true)
+	if diag == null:
+		set_process(false)
+		diag = null
+		return
+		
+	# This code snippet ensures only 1 dialogue is active
+	# At a time
+	if current_dialogue != null:
+		current_dialogue.unload()
+		
+	current_dialogue = diag
+	current_target = diag.entity
+	set_process(true)
 
 func _process(delta: float) -> void:
-    var dist_sqr = _player.global_position\
-        .distance_squared_to(current_target.global_position)
-    if dist_sqr >= 2800:
-        current_dialogue.unload()
+	var dist_sqr = _player.global_position\
+		.distance_squared_to(current_target.global_position)
+	if dist_sqr >= 2800:
+		current_dialogue.unload()
